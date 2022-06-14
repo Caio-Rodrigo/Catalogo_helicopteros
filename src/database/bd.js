9f37db6a079f535a2a00e882e1c6d125d7280050
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-06-13 23:34:03 -0300

    ssl

--- src/database/bd.js
+++ src/database/bd.js
@@ -2,6 +2,12 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = connection;
